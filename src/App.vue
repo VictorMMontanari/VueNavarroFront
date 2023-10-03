bc2ef8158ac6b4a55b91f0364fc41ae45f3fe9ca
Victor M <victormm9000@gmail.com>
2023-10-03 17:48:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+  <header class="cabecalho">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand">DDD</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link" aria-current="page">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <div class="main">
+    <RouterView></RouterView>
+  </div>  
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,100 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="principal">
+    <div class="container-fluid">
+      <div class="row justify-content-center align-items-center" style="margin-top: 2rem;">
+        <div class="card" style="width: 60rem;">
+          <div class="card-body">
+            <div class="container px-4 text-center">
+              <div class="row gx-5">
+                <div class="col-md-4 col-12">
+                  <div class="p-3">
+                    <router-link to="/aluno" class="nav-link">
+                      <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Aluno</div>
+                        <div class="card-body">
+                          <h5 class="card-title">CRUD Aluno</h5>
+                          <p class="card-text">Clique neste Card para acessar o Aluno</p>
+                        </div>
+                      </div>
+                    </router-link>
+                  </div>
+                </div>
+                <div class="col-md-4 col-12">
+                  <div class="p-3">
+
+                    <div class="card text-bg-secondary mb-3" style="max-width: 18rem;">
+                      <div class="card-header">Disciplina</div>
+                      <div class="card-body">
+                        <h5 class="card-title">CRUD Disciplina</h5>
+                        <p class="card-text">Clique neste Card para acessar o Disciplina</p>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+                <div class="col-md-4 col-12">
+                  <div class="p-3">
+
+                    <div class="card text-bg-success mb-3" style="max-width: 18rem;">
+                      <div class="card-header">Gerente</div>
+                      <div class="card-body">
+                        <h5 class="card-title">CRUD Gerente</h5>
+                        <p class="card-text">Clique neste Card para acessar o Gerente</p>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+              <div class="row gx-5">
+                <div class="col-md-4 col-12">
+                  <div class="p-3">
+
+                    <div class="card text-bg-danger mb-3" style="max-width: 18rem;">
+                      <div class="card-header">Matricula</div>
+                      <div class="card-body">
+                        <h5 class="card-title">Matricula</h5>
+                        <p class="card-text">Clique neste Card para acessar Matricula</p>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+                <div class="col-md-4 col-12">
+                  <div class="p-3">
+
+                    <div class="card text-bg-warning mb-3" style="max-width: 18rem;">
+                      <div class="card-header"> Programador </div>
+                      <div class="card-body">
+                        <h5 class="card-title">CRUD Programador</h5>
+                        <p class="card-text">Clique neste Card para acessar o Programador</p>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+                <div class="col-md-4 col-12">
+                  <div class="p-3">
+
+                    <div class="card text-bg-info mb-3" style="max-width: 18rem;">
+                      <div class="card-header">ProjetoTI</div>
+                      <div class="card-body">
+                        <h5 class="card-title">ProjetoTI</h5>
+                        <p class="card-text">Clique neste Card para acessar ProjetoTI</p>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
