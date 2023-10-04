b7642a767287d883b0120437fe28771ad1dfdb1b
VictorMmontanari <victormm9000@gmail.com>
2023-10-04 18:05:52 -0300

    PostAluno

--- src/views/AlunoView.vue
+++ src/views/AlunoView.vue
@@ -1,15 +1,184 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const formData = ref({
+  nome: '',
+  sobrenome: '',
+  email: '',
+  login: '',
+  senha: '',
+  dataCadastro: new Date().toISOString(), // Data atual em formato ISO
+  ativo: true,
+});
+
+axios.interceptors.request.use((config) => {
+  console.log('Dados a serem enviados:', config.data);
+  return config;
+});
+
+const enviarFormulario = () => {
+  axios.post('https://localhost:7127/api/aluno', formData.value, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      console.log('Resposta da API:', response.data);
+
+      // Limpar o formulário
+      formData.value.nome = '';
+      formData.value.sobrenome = '';
+      formData.value.email = '';
+      formData.value.login = '';
+      formData.value.senha = '';
+
+      // Exibir o modal de sucesso
+      const successModal = new bootstrap.Modal(document.getElementById('successModal'));
+      successModal.show();
+    })
+    .catch(error => {
+      console.error('Erro ao enviar formulário:', error);
+    });
+};
+
+</script>
+
 <template>
-    <div class="about">
-      <h1>Aluno</h1>
+  <main class="principal">
+    <div class="container-fluid conteudo2">
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+              POST Aluno
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <form class="row" @submit.prevent="enviarFormulario">
+                <div class="col-md-6">
+                  <label for="inputFirst" class="form-label">Nome</label>
+                  <input type="text" class="form-control" id="inputFirst" v-model="formData.nome">
+                </div>
+                <div class="col-md-6">
+                  <label for="inputLast" class="form-label">Sobrenome</label>
+                  <input type="text" class="form-control" id="inputLast" v-model="formData.sobrenome">
+                </div>
+                <div class="col-12">
+                  <label for="inputEmail4" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="inputEmail4" v-model="formData.email">
+                </div>
+                <div class="col-md-6">
+                  <label for="inputLogin4" class="form-label">Login</label>
+                  <input type="email" class="form-control" id="inputLogin4" v-model="formData.login">
+                </div>
+                <div class="col-md-6">
+                  <label for="inputPassword4" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="inputPassword4" v-model="formData.senha">
+                </div>
+                <input type="hidden" id="inputDate" v-model="formData.dataCadastro">
+                <div class="col-md-4">
+                  <label for="inputState" class="form-label">State</label>
+                  <select id="inputState" class="form-select" v-model="formData.ativo">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                  </select>
+                </div>
+                <div class="col-12">
+                  <br>
+                  <button type="submit" class="btn btn-primary">Cadastrar</button>
+                </div>
+              </form>
+              <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="successModalLabel">Sucesso</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+      </div>
+      <div class="modal-body">
+        Seu formulário foi enviado com sucesso!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fechar</button>
+      </div>
     </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-  </style>
+  </div>
+</div>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Accordion Item #2
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse
+              plugin adds the appropriate classes that we use to style each element. These classes control the overall
+              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
+              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+              <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Accordion Item #3
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin
+              adds the appropriate classes that we use to style each element. These classes control the overall
+              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
+              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+              <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseFor" aria-expanded="false" aria-controls="collapseFor">
+              Accordion Item #4
+            </button>
+          </h2>
+          <div id="collapseFor" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin
+              adds the appropriate classes that we use to style each element. These classes control the overall
+              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
+              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+              <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+.conteudo2 {
+  position: relative;
+  padding: 10px;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
+  margin-bottom: 10px;
+  border-radius: 10px;
+}
+
+.principal {
+  grid-area: principal;
+  height: calc(100vh - 110px);
+  background-color: #f0f0f0;
+  padding: 20px;
+}
+</style>
